0ac191158b0298abe301cbcb6566f5a40067935f
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-09-03 18:55:11 +0600

    cards added

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 fetch("https://openapi.programming-hero.com/api/news/categories")
-.then(res => res.json())
-.then(data => navItem(data.data.news_category))
+    .then(res => res.json())
+    .then(data => navItem(data.data.news_category))
 
 
-function navItem(data){
-    for (category of data){
+function navItem(data) {
+    for (category of data) {
         const span = document.createElement("span");
         const categoryContainer = document.getElementById("category");
         span.innerText = category.category_name;
-        span.classList.add("text-secondary", "fs-5", "pointer")
-        span.addEventListener("click", ()=>{
-            
+        span.classList.add("text-secondary", "fs-5", "pointer");
+        span.addEventListener("click", () => {
             fetch(`https://openapi.programming-hero.com/api/news/category/${category.category_id}`)
-            .then(res => res.json())
-            .then(data => displayNews(data.data))
-            function displayNews(data){}
+                .then(res => res.json())
+                .then(data => displayNews(data.data))
         }, true);
-        categoryContainer.appendChild(span)
+        categoryContainer.appendChild(span);
+    }
+}
+
+function displayNews(data) {
+    const newsContainer = document.getElementById("news-container");
+    newsContainer.textContent = "";
+    for (news of data){
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card mb-5">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src=${news.image_url} class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text text-secondary overflow-ellipsis">${news.details}</p>
+                    </div>
+                    <div class="ms-3">
+                        <div class="d-flex justify-content-start">
+                        <img src= ${news.author.img} class="rounded-circle author-img" alt="...">
+                        <div class="ms-2">
+                        <p class="mb-0">${news.author.name}</p>
+                        <p class="mb-0">${news.author.published_date}</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        div.classList.add("container")
+        newsContainer.appendChild(div)
     }
 }
