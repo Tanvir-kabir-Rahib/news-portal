96fe165e50e90b23b389f523904f356d9a75ea82
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-09-03 22:09:30 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -15,12 +15,12 @@ function navItem(data) {
         categoryContainer.appendChild(span);
         span.addEventListener("click", () => {
             fetch(`https://openapi.programming-hero.com/api/news/category/${category.category_id}`)
-            .then(res => res.json())
-            .then(data => displayNews(data.data))
-            .catch((error) => {
-                console.log(error)
-            });
-            spinner(true)
+                .then(res => res.json())
+                .then(data => displayNews(data.data))
+                .catch((error) => {
+                    console.log(error);
+                });
+            spinner(true);
         });
     });
 }
@@ -62,19 +62,71 @@ function displayNews(data) {
         </div>
     </div>
         `
-        div.classList.add("container")
-        newsContainer.appendChild(div)
+        div.classList.add("container");
+        div.setAttribute("data-bs-target","#exampleModal")
+        div.setAttribute("data-bs-toggle","modal")
+        newsContainer.appendChild(div);
+        div.addEventListener("click", () => {
+            fetch(`https://openapi.programming-hero.com/api/news/${news._id}`)
+                .then(res => res.json())
+                .then(data => loadNews(data.data[0]))
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
 
     });
     spinner(false)
 }
 
+function loadNews(data) {
+    const modalSection = document.getElementById("modal-section");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header flex-column">
+                <img src=${data.image_url} class="img-fluid" alt="">
+                <h5 class="modal-title mt-3" id="exampleModalLabel">${data.title}</h5>
+            </div>
+            <div class="modal-body">
+                <div>
+                    ${data.details}
+                </div>
+                <div class="d-flex align-items-center mt-3">
+                    <i class="fa-regular fa-eye pe-2 fs-3"></i>
+                    <span class="fs-5">${data.total_view}</span>
+                </div>
+                <div class="d-flex align-items-center mt-3">
+                    <i class="fa-solid fa-star me-2 fs-3"></i>
+                    <span class="fs-5">${(data.rating.number)? data.rating.number : "Not Available"}</span>
+                </div>
+                <div class="d-flex justify-content-start mt-3">
+                    <img src=${(data.author.img)? data.author.img : "Not Available"} class="rounded-circle author-img" alt="...">
+                    <div class="ms-2">
+                        <p class="mb-0">${(data.author.name)? data.author.name : "Not Available"}</p>
+                        <p class="mb-0">${(data.author.published_date)? data.author.published_date : "Not Available"}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+  `
+    modalSection.appendChild(div)
+}
+
 function spinner(spin) {
     const loader = document.getElementById("loader");
     if (spin) {
-        loader.classList.remove("d-none");
+        loader?.classList.remove("d-none");
     }
     else {
-        loader.classList.add("d-none")
+        loader?.classList.add("d-none");
     }
 }
