ec32ae7a42cd4e428c9a1431730a0bf124a22831
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-09-04 11:11:41 +0600

    modal error solved

--- js/app.js
+++ js/app.js
@@ -91,49 +91,43 @@ const displayNews =(data, name)=> {
 }
 
 const loadNews = (data) => {
-    const modalSection = document.getElementById("modal-section");
+    const modalSection = document.getElementById("modal-content");
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header flex-column">
-                <img src=${data.image_url} class="img-fluid" alt="">
-                <h5 class="modal-title mt-3" id="staticBackdropLabel">${data.title}</h5>
-            </div>
-            <div class="modal-body">
-                <div>
-                    ${data.details}
-                </div>
-                <div class="d-flex align-items-center mt-3">
-                    <i class="fa-regular fa-eye pe-2 fs-3"></i>
-                    <span class="fs-5">${(data.total_view)? data.total_view : "Not Available"}</span>
-                </div>
-                <div class="d-flex align-items-center mt-3">
-                    <i class="fa-solid fa-star me-2 fs-3"></i>
-                    <span class="fs-5">${(data.rating.number)? data.rating.number : "Not Available"}</span>
-                </div>
-                <div class="d-flex justify-content-start mt-3">
-                    <img src=${(data.author.img)? data.author.img : "Not Available"} class="rounded-circle author-img" alt="...">
-                    <div class="ms-2">
-                        <p class="mb-0">${(data.author.name)? data.author.name : "Not Available"}</p>
-                        <p class="mb-0">${(data.author.published_date)? data.author.published_date : "Not Available"}</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal-footer">
-                <button onclick="clearModal()" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
+    <div class="modal-header flex-column">
+    <img src=${data.image_url} class="img-fluid" alt="">
+    <h5 class="modal-title mt-3" id="staticBackdropLabel">${data.title}</h5>
+    </div>
+    <div class="modal-body">
+    <div>
+        ${data.details}
+    </div>
+    <div class="d-flex align-items-center mt-3">
+        <i class="fa-regular fa-eye pe-2 fs-3"></i>
+        <span class="fs-5">${(data.total_view)? data.total_view : "Not Available"}</span>
+    </div>
+    <div class="d-flex align-items-center mt-3">
+        <i class="fa-solid fa-star me-2 fs-3"></i>
+        <span class="fs-5">${(data.rating.number)? data.rating.number : "Not Available"}</span>
+    </div>
+    <div class="d-flex justify-content-start mt-3">
+        <img src=${(data.author.img)? data.author.img : "Not Available" }
+            class="rounded-circle author-img" alt="...">
+        <div class="ms-2">
+            <p class="mb-0">${(data.author.name)? data.author.name : "Not Available"}</p>
+            <p class="mb-0">${(data.author.published_date)? data.author.published_date : "Not Available"}</p>
         </div>
     </div>
-</div>
+    </div>
+    <div class="modal-footer">
+    <button type="button" onclick="clearModal()" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
   `
     modalSection.appendChild(div);
 }
 
 const clearModal =()=> { 
-    const modalSection = document.getElementById("modal-section");
+    const modalSection = document.getElementById("modal-content");
     modalSection.textContent = ""
 }
 
